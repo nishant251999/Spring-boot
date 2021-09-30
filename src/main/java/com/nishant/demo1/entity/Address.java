929b0a7fe3a8689e289aad4d065dc760ab8a09db
nishant <nishant25dev@gmail.com>
2021-09-30 18:02:11 +0530

    CRUD with mySQL w/o address working

--- src/main/java/com/nishant/demo1/entity/Address.java
+++ src/main/java/com/nishant/demo1/entity/Address.java
@@ -1,7 +1,16 @@
 package com.nishant.demo1.entity;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
     
+    @Id
+    @GeneratedValue
+    private long address_id;
     private String addLine1;
     private String addLine2;
     private String city;
@@ -10,49 +19,64 @@ public class Address {
 
     public Address() {
     }
-    
-    public Address(String addLine1, String addLine2, String city, String country, long pincode) {
+
+    public Address(long address_id, String addLine1, String addLine2, String city, String country, long pincode) {
+        this.address_id = address_id;
         this.addLine1 = addLine1;
         this.addLine2 = addLine2;
         this.city = city;
         this.country = country;
         this.pincode = pincode;
+    }
+
+
+    public long getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(long address_id) {
+        this.address_id = address_id;
     }
 
     public String getAddLine1() {
         return addLine1;
     }
+
     public void setAddLine1(String addLine1) {
         this.addLine1 = addLine1;
     }
+
     public String getAddLine2() {
         return addLine2;
     }
+
     public void setAddLine2(String addLine2) {
         this.addLine2 = addLine2;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getCountry() {
         return country;
     }
+
     public void setCountry(String country) {
         this.country = country;
     }
+
     public long getPincode() {
         return pincode;
     }
+
     public void setPincode(long pincode) {
         this.pincode = pincode;
     }
-
-    @Override
-    public String toString() {
-        return "Address [addLine1=" + addLine1 + ", addLine2=" + addLine2 + ", city=" + city + ", country=" + country
-                + ", pincode=" + pincode + "]";
-    }
+    
+    
 }
